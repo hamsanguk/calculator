c817ad9d6f5039aa5df21c8cca861564cbaac333
hamsanguk <gtd5309@gmail.com>
2025-02-18 18:08:15 +0900

    blog upload

--- script.js
+++ script.js
@@ -6,12 +6,23 @@ const operator = document.querySelector('.calculator__operator'); // calculator_
 const secondOperend = document.querySelector('.calculator__operend--right'); // calculator__operend--right 엘리먼트와, 그 자식 엘리먼트의 정보를 모두 담고 있습니다.
 const calculatedResult = document.querySelector('.calculator__result'); // calculator__result 엘리먼트와, 그 자식 엘리먼트의 정보를 모두 담고 있습니다.
 
-function calculate(n1, operator, n2) {
+
+function calculate(n1, oper, n2) { // n1 = 1, oper = +, n2 = 2
   let result = 0;
-  // TODO : n1과 n2를 operator에 따라 계산하는 함수를 만드세요.
-  // ex) 입력값이 n1 : '1', operator : '+', n2 : '2' 인 경우, 3이 리턴됩니다.
+
+  if(oper === '+'){
+    result = Number(n1) + Number(n2)
+  }else if(oper === '-'){
+    result = Number(n1) - Number(n2)
+  }else if(oper === '*'){
+    result = Number(n1) * Number(n2)
+  }else{
+    result = Number(n1) / Number(n2)
+  }
+  
   return String(result);
-}
+};
+
 
 buttons.addEventListener('click', function (event) {
   // 버튼을 눌렀을 때 작동하는 함수입니다.
@@ -25,25 +36,40 @@ buttons.addEventListener('click', function (event) {
     // TODO : 계산기가 작동할 수 있도록 아래 코드를 수정하세요. 작성되어 있는 조건문과 console.log를 활용하시면 쉽게 문제를 풀 수 있습니다.
     // 클릭된 HTML 엘리먼트가 button이면
     if (action === 'number') {
-      // 그리고 버튼의 클레스가 number이면
-      // 아래 코드가 작동됩니다.
-      console.log('숫자 ' + buttonContent + ' 버튼');
+      if(firstOperend.textContent ==='0'){
+        firstOperend.textContent = buttonContent;
+      }else{
+        secondOperend.textContent = buttonContent; //두번째 operend 입력
+      }
     }
 
     if (action === 'operator') {
-      console.log('연산자 ' + buttonContent + ' 버튼');
+      operator.textContent = buttonContent;
     }
 
     if (action === 'decimal') {
-      // console.log('소수점 버튼');
+       console.log('소수점 버튼');
     }
 
     if (action === 'clear') {
       console.log('초기화 버튼');
+      firstOperend.textContent ='0'
+      secondOperend.textContent = '0'
+      operator.textContent='+'
+      calculatedResult.textContent='0';
     }
 
     if (action === 'calculate') {
-      console.log('계산 버튼');
+      let firstOperTextContent=firstOperend.textContent,
+          secondOperTextContent=secondOperend.textContent,
+          operTextcontent=operator.textContent;
+
+      const result = calculate(
+        firstOperTextContent,
+        operTextcontent,
+        secondOperTextContent
+      );
+      calculatedResult.textContent = result;
     }
   }
 });
@@ -52,10 +78,22 @@ buttons.addEventListener('click', function (event) {
 // ! Advanced Challenge test와 Nightmare test를 위해서는 아래 주석을 해제하세요.
 
 const display = document.querySelector('.calculator__display--for-advanced'); // calculator__display 엘리먼트와, 그 자식 엘리먼트의 정보를 모두 담고 있습니다.
+const clickedOperator = document.querySelectorAll('.operator');
+
+clickedOperator.forEach((button) => {
+  button.addEventListener('click', () => {
+    button.classList.add('button_clicked');
+
+    setTimeout(() => {
+      button.classList.remove('button_clicked');
+    }, 200);
+  });
+});
+
 let firstNum, operatorForAdvanced, previousKey, previousNum;
 
+
 buttons.addEventListener('click', function (event) {
-  // 버튼을 눌렀을 때 작동하는 함수입니다.
 
   const target = event.target; // 클릭된 HTML 엘리먼트의 정보가 저장되어 있습니다.
   const action = target.classList[0]; // 클릭된 HTML 엘리먼트에 클레스 정보를 가져옵니다.
@@ -64,11 +102,36 @@ buttons.addEventListener('click', function (event) {
 
   // ! 여기서부터 Advanced Challenge & Nightmare 과제룰 풀어주세요.
   if (target.matches('button')) {
-    if (action === 'number') {}
-    if (action === 'operator') {}
+    if (action === 'number') {
+      if(display.textContent==="0" || previousKey === 'calculate'){
+        display.textContent = ''
+      }
+      display.textContent += buttonContent;
+    }
+    if (action === 'operator') {
+      operatorForAdvanced = buttonContent;
+      previousKey = 'operator'
+      firstNum =display.textContent;
+
+      if (operatorForAdvanced === "*" || display.textContent === '0') {
+        display.textContent = buttonContent;
+      }else if(operatorForAdvanced === "+" || display.textContent === '0') {
+        display.textContent = buttonContent;
+      }else if(operatorForAdvanced === "-" || display.textContent === '0') {
+        display.textContent = buttonContent;
+      }else if(operatorForAdvanced === "/" || display.textContent === '0') {
+        display.textContent = buttonContent;
+      }
+    }
     if (action === 'decimal') {}
-    if (action === 'clear') {}
-    if (action === 'calculate') {}
+    if (action === 'clear') {
+      display.textContent = '0'
+    }
+    if (action === 'calculate') {
+      console.log(firstNum,operatorForAdvanced,display.textContent)
+      display.textContent = calculate(firstNum,operatorForAdvanced,display.textContent.slice(1));
+      previousKey='calculate';
+    }
   }
 
 });
